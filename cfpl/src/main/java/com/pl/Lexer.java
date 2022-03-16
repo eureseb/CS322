113497a3bcfbd5f2bdff64aac19f193bb64d7fbd
Eurese Antonio Bustamante <eurese.bustamante@orangeandbronze.com>
2022-03-16 20:04:26 +0800

    Added parseAlpha code

--- cfpl/src/main/java/com/pl/Lexer.java
+++ cfpl/src/main/java/com/pl/Lexer.java
@@ -1,5 +1,17 @@
 package com.pl;
 
+import static com.pl.Tokens.*;
+
 class Lexer {
     
+    Tokens parseAlpha(String token){
+        Tokens type = null;
+        if (token.equals("VAR")) {
+            // type = KW_VAR;
+        }
+        else if (token.equals("OUTPUT")) {
+            // type = KW_OUTPUT;
+        }
+        return type;
+    }
 }
